40cc55421278699c341baa056fac79207ad2c9df
kienvu <ciencute1998@gmail.com>
2022-04-12 01:43:01 +0700

    setup api

--- app_mobile/lib/base/di.dart
+++ app_mobile/lib/base/di.dart
@@ -1,12 +1,17 @@
+import '../api/datasource/movie_data_api.dart';
+import '../api/repositories/movie_app_repository.dart';
+import '../api/repositories/movie_app_repository_impl.dart';
+import 'package:get/get.dart';
 
 import '../api/network/dio_client.dart';
 import '../shared/constants/common.dart';
 
-
 class DependencyInjection {
   static Future<void> init(String environment) async {
     //api
-    final _dioAPIClient =
-        await DioClient.setup(baseUrl: ClientAPIDomain);
+    final _dioAPIClient = await DioClient.setup(baseUrl: ClientAPIDomain);
+    final uiAPI = MovieAPI(_dioAPIClient);
+    final MovieAppRepository uiRepo = MovieAppRepositoryImpl(uiAPI);
+    Get.put(uiRepo);
   }
 }
